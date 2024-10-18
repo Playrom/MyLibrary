cdb27ef443657e86e758095864efb4f9a2bbebb4
Giorgio Romano <lavoro@giorgioromano.it>
2024-10-18 17:16:53 +0200

    Add mysub

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,6 +1,8 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import MySubLibrary
+
 public let hello: String = "Hello, World!"
 public let fooBar: String = "Foo Bar"
-public let miao: Int = 3
+public let miao: Int = mySub
